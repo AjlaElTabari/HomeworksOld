76c415c3dde18fa33ab0601de81aae26096f5532
AjlaElTabari <ajla.eltabari@bitcamp.ba>
2015-06-10 01:01:28 +0200

    Homework#13, Task3 modification

--- src/ba/BITCamp/ajla/Homework13/Task3.java
+++ src/ba/BITCamp/ajla/Homework13/Task3.java
@@ -4,24 +4,27 @@ public class Task3 {
 
 	public static void main(String[] args) {
 		int counter = 0;
-		if (args.length > 1) {
-			throw new ArrayIndexOutOfBoundsException("Please pass only one String to the program.");
-		}
+		
 		try {
+			if (args.length > 1) {
+			throw new ArrayIndexOutOfBoundsException("Please pass only one String to the program.");
+			}
 			for(int i = 0; i < args[0].length(); i++) {
 				if (Character.isDigit(args[0].charAt(i))) {
 					counter++;
 				}
 			}
+			
+			if (counter == 0) {
+				System.out.println(args[0] + " does not contain any numbers");
+			} else {
+				System.out.println(args[0] + " contains " + counter + " numbers");
+			}
 		} catch(ArrayIndexOutOfBoundsException e) {
 			System.out.println("Please pass one String to the program.");
 		}
 
-		if (counter == 0) {
-			System.out.println(args[0] + " does not contain any numbers");
-		} else {
-			System.out.println(args[0] + " contains " + counter + " numbers");
-		}
+
 		
 	}
 
